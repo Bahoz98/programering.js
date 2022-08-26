0263294d00d1be480aafa6f661ba02d507b1d440
Bahoz Mosa Mahmoud <bahoz.mosamahmoud@learnet.se>
2022-08-26 15:53:33 +0200

    hej

---
+++ programering.js
@@ -0,0 +1,7 @@
+//console.log("hello World")
+//console.log("once upon a time")
+//console.log("i was falling in love")
+//console.log("now i'm only falling apart")
+//console.log("once upon a time i was falling in love now i'm only fallin
+var text = "Live and sleep";
+console.log(text);
